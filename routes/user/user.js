d6d29608c21646bb117ab5be2923050b685ac090
pkg4446 <pkg4446@gmail.com>
2022-07-07 17:01:04 +0900

    220707

--- routes/user/user.js
+++ routes/user/user.js
@@ -5,7 +5,7 @@ const router  = express.Router();
 
 router.post('/login',async function(req, res, next) {
     const response = {
-        result: false,
+        result: true,
         data:   null,
     }
     try {        
@@ -14,12 +14,41 @@ router.post('/login',async function(req, res, next) {
             USER_PASS:  req.body.USER_PASS
         }
         const pass = await user.passCheck(data);
-        if(pass){
-            response.result = true;
+        if(pass){            
             response.data   = await user.info(data);
+        }else{
+            response.result = false;
         }
     } catch (error) {   
-        next(error);     
+        response.result = false; 
+        next(error);    
+    }     
+    res.json(response);
+});
+
+router.post('/join',async function(req, res, next) {
+    const response = {
+        result: true,
+        data:   null,
+    }
+    for(const key of Object.keys(req.body)){
+        if(req.body[key]==""){
+            response.result = false;
+            response.data   = "null";
+        }
+    }
+    try {        
+        if(response.data){
+            const pass = await user.passCheck(data);
+            if(pass){            
+                response.data   = await user.info(data);
+            }else{
+                response.result = false;
+            }
+        }
+    } catch (error) {   
+        response.result = false; 
+        next(error);    
     }     
     res.json(response);
 });
