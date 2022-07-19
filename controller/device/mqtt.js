17cadf07a31bcd5a12a5f4185b8ff5c0f70763df
pkg4446 <pkg4446@gmail.com>
2022-07-19 15:40:18 +0900

    220719

--- controller/device/mqtt.js
+++ controller/device/mqtt.js
@@ -48,57 +48,62 @@ client.on("message", async(topic, message) => {
                     case "AT+manual":                    
                         await devices.plasma_update(device[0],"METHOD",3);
                         break;
-                    case "AT+sTshD":                        
-                        await devices.plasma_update(device[0],"A_S_H",device[2]);
-                        break;
-                    case "AT+sTsmD":
-                        await devices.plasma_update(device[0],"A_S_M",device[2]);                    
-                        break;
-                    case "AT+sTehD":
-                        await devices.plasma_update(device[0],"A_E_H",device[2]);
-                        break;
-                    case "AT+sTemD":
-                        await devices.plasma_update(device[0],"A_E_M",device[2]);
-                        break;
-                    case "AT+sTshN":                        
-                        await devices.plasma_update(device[0],"P_S_H",device[2]);
-                        break;
-                    case "AT+sTsmN":
-                        await devices.plasma_update(device[0],"P_S_M",device[2]);                    
-                        break;
-                    case "AT+sTehN":
-                        await devices.plasma_update(device[0],"P_E_H",device[2]);
-                        break;
-                    case "AT+sTemN":
-                        await devices.plasma_update(device[0],"P_E_M",device[2]);
-                        break;
-                
-                    case "AT+sTon":                        
-                        await devices.plasma_update(device[0],"T_ON",device[2]);
-                        break;
-                    case "AT+sToff":
-                        await devices.plasma_update(device[0],"T_OFF",device[2]);                    
-                        break;
-                    case "AT+sTPD":
-                        await devices.plasma_update(device[0],"P_D",device[2]);
-                        break;
-                    case "AT+sTPR":
-                        await devices.plasma_update(device[0],"P_T",device[2]);
-                        break;
-                    case "AT+CO2":                        
-                        await devices.plasma_update(device[0],"S_CO2",device[2]);
-                        break;
-                    case "AT+NH3":
-                        await devices.plasma_update(device[0],"S_NH3",device[2]);                    
-                        break;
-                    case "AT+H2S":
-                        await devices.plasma_update(device[0],"S_H2S",device[2]);
-                        break;
-                    case "AT+Heat":
-                        await devices.plasma_update(device[0],"H_T",device[2]);
-                        break;
                     default:
                         break;
+                }
+                if(device[2]){
+                    switch (device[1]) {                   
+                        case "AT+sTshD":                        
+                            await devices.plasma_update(device[0],"A_S_H",device[2]);
+                            break;
+                        case "AT+sTsmD":
+                            await devices.plasma_update(device[0],"A_S_M",device[2]);                    
+                            break;
+                        case "AT+sTehD":
+                            await devices.plasma_update(device[0],"A_E_H",device[2]);
+                            break;
+                        case "AT+sTemD":
+                            await devices.plasma_update(device[0],"A_E_M",device[2]);
+                            break;
+                        case "AT+sTshN":                        
+                            await devices.plasma_update(device[0],"P_S_H",device[2]);
+                            break;
+                        case "AT+sTsmN":
+                            await devices.plasma_update(device[0],"P_S_M",device[2]);                    
+                            break;
+                        case "AT+sTehN":
+                            await devices.plasma_update(device[0],"P_E_H",device[2]);
+                            break;
+                        case "AT+sTemN":
+                            await devices.plasma_update(device[0],"P_E_M",device[2]);
+                            break;                
+                        case "AT+sTon":                        
+                            await devices.plasma_update(device[0],"T_ON",device[2]);
+                            break;
+                        case "AT+sToff":
+                            await devices.plasma_update(device[0],"T_OFF",device[2]);                    
+                            break;
+                        case "AT+sTPD":
+                            await devices.plasma_update(device[0],"P_D",device[2]);
+                            break;
+                        case "AT+sTPR":
+                            await devices.plasma_update(device[0],"P_T",device[2]);
+                            break;
+                        case "AT+CO2":                        
+                            await devices.plasma_update(device[0],"S_CO2",device[2]);
+                            break;
+                        case "AT+NH3":
+                            await devices.plasma_update(device[0],"S_NH3",device[2]);                    
+                            break;
+                        case "AT+H2S":
+                            await devices.plasma_update(device[0],"S_H2S",device[2]);
+                            break;
+                        case "AT+Heat":
+                            await devices.plasma_update(device[0],"H_T",device[2]);
+                            break;
+                        default:
+                            break;
+                    }
                 }
             }            
         }
