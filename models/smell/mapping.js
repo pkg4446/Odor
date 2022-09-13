8430aa030c5ff63bc637a4738dd8a7f9eadf7e1e
pkg4446 <pkg4446@gmail.com>
2022-09-13 16:41:44 +0900

    220913

--- models/smell/mapping.js
+++ models/smell/mapping.js
@@ -12,7 +12,6 @@ module.exports = class mapping extends Sequelize.Model{
             TYPE: {
                 type: Sequelize.BOOLEAN,
                 primaryKey: true,
-                autoIncrement: true
             },
             //가우시안 퍼퓸 결과값을 넣기 위해서 센서의 경우 센서 인덱스를 받아와야함(원뿔 형태로 퍼짐 표현하기위해 지름이 다양한 원을 여럿 표시.)
             //위도
