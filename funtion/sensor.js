b58bac396c5e0f527f0da9a865588f276f35b500
pkg4446 <pkg4446@gmail.com>
2022-10-04 11:17:59 +0900

    221004

--- funtion/sensor.js
+++ funtion/sensor.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+const { DATE } = require('sequelize');
 const Library   = require('../controller/application/report');
 
 module.exports  = {
@@ -8,6 +10,94 @@ module.exports  = {
       console.error(err);
     }
   },//report
+  test : async function(){    
+    weather();
+  },//report
+}
+
+function format(value){
+  let response = value;
+  if(value<10) response = `0${value}`
+  return response;
+}
+
+async function weather(GPS_LATITUDE,GPS_LONGITUDE){
+  GPS_LATITUDE  = 55;
+  GPS_LONGITUDE = 127;
+  const axios = require('axios');
+  const today = new Date();
+  const now   = `${today.getFullYear()}${format(today.getMonth()+1)}${format(today.getDate())}`; 
+  let time    = today.toTimeString().split(' ');  
+  time        = time[0].split(':');
+  time        = `${format(time[0]-1)}${format(time[1])}`
+  const data  = {
+                serviceKey: process.env.gokey,
+                numOfRows:  10,
+                pageNo:     1,
+                base_date:  now,
+                base_time:  time,
+                nx:         GPS_LATITUDE,
+                ny:         GPS_LONGITUDE,
+                dataType:   "JSON"
+  }
+
+  const URL = `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=${data.serviceKey}&base_date=${data.base_date}&base_time=${data.base_time}&nx=${data.nx}&ny=${data.ny}&dataType=JSON`
+
+  const weatherData = {
+    GPS_LATITUDE:   GPS_LATITUDE,
+    GPS_LONGITUDE:  GPS_LONGITUDE,
+    PTY:  null,
+    REH:  null,
+    RN1:  null,
+    T1H:  null,
+    UUU:  null,
+    VEC:  null,
+    VVV:  null,
+    WSD:  null
+  }
+
+  await axios({
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+    },
+    method: "get", // 요청 방식
+    url: URL // 요청 주소
+  })
+  .then(function(res){
+    const response = res.data.response.body.items.item;    
+    for(let item of response){
+      switch (item.category) {
+        case "PTY":
+          weatherData.PTY=item.obsrValue;
+          break;
+        case "REH":
+          weatherData.REH=item.obsrValue;      
+          break;
+        case "RN1":
+          weatherData.RN1=item.obsrValue; 
+          break;
+        case "T1H":
+          weatherData.T1H=item.obsrValue; 
+          break;
+        case "UUU":
+          weatherData.UUU=item.obsrValue; 
+          break;
+        case "VEC":
+          weatherData.VEC=item.obsrValue; 
+          break;
+        case "VVV":
+          weatherData.VVV=item.obsrValue; 
+          break;
+        case "WSD":
+          weatherData.WSD=item.obsrValue; 
+          break;                                                
+        default:
+          break;
+      }
+    }          
+  });
+  console.log(weatherData);  
+  return weatherData;
 }
 
 function area(data){
